fc45c3baa9629141dbdc74c439adbbba6d39d6eb
Wenxi Zeng <wenxi.zeng@outlook.com>
2020-04-13 16:38:57 -0500

    remove test output code

--- src/main/java/managers/FileWorker.java
+++ src/main/java/managers/FileWorker.java
@@ -36,46 +36,27 @@ public class FileWorker implements Runnable{
         while (!Thread.interrupted()) {
             try {
 
-                SimpleLog.v("run 1.........................");
                 while (queue.isEmpty() ||
                         !queue.peek().getSender().equals(Config.getInstance().getAddress()) ||
                         !ackFromAll(queue.peek())) {
-                    if (queue.isEmpty()) SimpleLog.v("run semaphore sender check, queue is empty");
-                    else
-                        SimpleLog.v("run semaphore sender check, sender: " + queue.peek().getSender() + ", self: " + Config.getInstance().getAddress());
                     semaphore.acquire();
-                    SimpleLog.v("run semaphore release");
                 }
 
-                SimpleLog.v("run 2.........................");
                 if (!queue.isEmpty()) {
                     if (queue.peek().getSender().equals(Config.getInstance().getAddress()))
                         operateFile(queue.poll());
-                    else
-                        SimpleLog.v("run sender check, sender: " + queue.peek().getSender() + ", self:" + Config.getInstance().getAddress());
                 }
-                else
-                    SimpleLog.v("queue is empty");
-
-                SimpleLog.v("run 3.........................");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
-        SimpleLog.v("thread stop working");
     }
 
     private void operateFile(Request request) {
-        SimpleLog.v("operateFile " + request);
         try {
             FileHelper.append(Config.getInstance().getId(), request.getHeader(), request.getAttachment());
             if (request.getProcessed() != null) {
                 request.getProcessed().release();
-                SimpleLog.v("operateFile release");
-            }
-            else {
-                SimpleLog.v("operateFile semaphore is null");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -83,26 +64,18 @@ public class FileWorker implements Runnable{
     }
 
     private boolean ackFromAll(Request request) {
-        if (clocks == null) {
-            SimpleLog.v("ackFromAll clock is null");
-            return false;
-        }
+        if (clocks == null) return false;
+
         List<Long> replicaClocks = new ArrayList<>();
         for (PhysicalNode node : LookupTable.getInstance().lookup(request.getHeader())) {
             if (clocks.containsKey(node.getAddress()))
                 replicaClocks.add(clocks.get(node.getAddress()));
         }
 
-        if (replicaClocks.isEmpty()) {
-            SimpleLog.v("ackFromAll no replicas");
-            return false;
-        }
+        if (replicaClocks.isEmpty()) return false;
 
         for (long clock : replicaClocks) {
-            if (request.getTimestamp() > clock) {
-                SimpleLog.v("ackFromAll not ack all, request: " + request.getTimestamp() + ", clock: " + clock);
-                return false;
-            }
+            if (request.getTimestamp() > clock) return false;
         }
 
         return true;
@@ -114,23 +87,16 @@ public class FileWorker implements Runnable{
     }
 
     public void release(Request request) {
-        SimpleLog.v("release, sender: " + request.getSender());
         if (!queue.isEmpty() &&
-                queue.peek().getSender().equals(request.getSender())) {
-
-            SimpleLog.v("release, sender is same");
+                queue.peek().getSender().equals(request.getSender()))
             queue.poll();
-        }
-        this.semaphore.release();
 
+        this.semaphore.release();
     }
 
     public void setClocks(Map<String, Long> clocks) {
-        SimpleLog.v("setClocks. clock updated. thread is working: " + isWorking());
-        SimpleLog.i(clocks);
         this.clocks = clocks;
         this.semaphore.release();
-        SimpleLog.i("setClocks done!");
     }
 
     public boolean isWorking() {
